e0dd21ff5a888e3d0ddec847d857c7684b1a70a3
Matthew Troke <trokematthew@gmail.com>
2026-05-26 17:09:01 -0700

    Removed made by

--- src/lib/components/sections/Footer.svelte
+++ src/lib/components/sections/Footer.svelte
@@ -1,8 +1,6 @@
 <footer class="footer" id="contact">
 	<div class="inner">
 		<a href="mailto:contact@dinofries.com" class="link">contact@dinofries.com</a>
-		<span class="sep" aria-hidden="true">~</span>
-		<a class="link" href="https://matthewtroke.com" target="_blank" rel="noopener noreferrer">made by Matthew Troke</a>
 	</div>
 </footer>
 
